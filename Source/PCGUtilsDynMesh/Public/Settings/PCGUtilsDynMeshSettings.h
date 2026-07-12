755b4d7c8ca83817a490f5164ab86cd4e6ebbff7
Max H <mharris382@gmail.com>
2026-07-12 12:01:30 -0400

    added sample tool for baking mesh pipeline

--- Source/PCGUtilsDynMesh/Public/Settings/PCGUtilsDynMeshSettings.h
+++ Source/PCGUtilsDynMesh/Public/Settings/PCGUtilsDynMeshSettings.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Engine/DeveloperSettings.h"
+#include "UObject/SoftObjectPtr.h"
 #include "PCGUtilsDynMeshSettings.generated.h"
 
 class UMaterialInterface;
@@ -16,9 +17,10 @@ class PCGUTILSDYNMESH_API UPCGUtilsDynMeshSettings : public UDeveloperSettings
 public:
 	UPCGUtilsDynMeshSettings();
 
+#if WITH_EDITOR
 	virtual FName GetCategoryName() const override;
 	virtual FText GetSectionText() const override;
-
+#endif
 	UPROPERTY(Config, EditAnywhere, Category="Brushes", meta=(AllowedClasses="/Script/Engine.MaterialInterface"))
 	TSoftObjectPtr<UMaterialInterface> StaticMeshBrushMaterial;
 };
